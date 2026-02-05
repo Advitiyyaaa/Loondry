6f1a56fc4c04c5df247e2a5f5744f5918a3bb8b0
Advitiya Arya <advitiya.arya@gmail.com>
2026-02-06 01:47:35 +0530

    changed baseURL

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -1,7 +1,14 @@
 import axios from "axios"
 
+const API_URL = import.meta.env.VITE_API_URL;
+
+if (!API_URL) {
+  throw new Error("VITE_API_URL is missing");
+}
+
 const axiosClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:2000',
+    
+    baseURL: API_URL || 'http://localhost:2000',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
