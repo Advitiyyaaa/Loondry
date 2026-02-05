cd47ff4f8691b3b2e9164730f8dc7ef5814e42e8
Advitiya Arya <advitiya.arya@gmail.com>
2026-02-06 02:03:33 +0530

    axiosClient setup

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -1,14 +1,7 @@
 import axios from "axios"
 
-const API_URL = import.meta.env.VITE_API_URL;
-
-if (!API_URL) {
-  throw new Error("VITE_API_URL is missing");
-}
-
 const axiosClient = axios.create({
-    
-    baseURL: API_URL || 'http://localhost:2000',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:2000',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
